1babed26288de09fb3e60efee36c720ce948303c
LuxksC <lucas_08_castro@hotmail.com>
2021-10-05 12:31:25 -0300

    create useRoom hook

--- letmeask/src/hooks/useRoom.ts
+++ letmeask/src/hooks/useRoom.ts
@@ -1,7 +1,55 @@
+import { useEffect, useState } from "react";
+import { database } from "../services/firebase";
 
+type QuestionType = {
+  id: string,
+  author: {
+    avatar: string;
+    name: string;
+  };
+  content: string;
+  isAnswered: boolean;
+  isHighLighted: boolean;
+}
 
-export function useRoom() {
-  return(
-    30
-  )
+type FirebaseQuestions = Record<string, {
+  author: {
+    avatar: string;
+    name: string;
+  };
+  content: string;
+  isAnswered: boolean;
+  isHighLighted: boolean;
+}>
+
+export function useRoom(roomId: string) {
+  const [questions, setQuestions] = useState<QuestionType[]>([])
+  const [title, setTitle] = useState('')
+
+  useEffect(() => {
+    const roomRef = database.ref(`rooms/${roomId}`);
+
+    roomRef.on('value', room => {
+      const databaseRoom = room.val();
+      const firebaseQuestions: FirebaseQuestions = databaseRoom.questions ?? {};
+
+      const parsedQuestions = Object.entries(firebaseQuestions).map(([key, value]) => {
+        return {
+          id: key,
+          author: value.author,
+          content: value.content,
+          isAnswered: value.isAnswered,
+          isHighLighted: value.isHighLighted,
+        }
+      })
+
+      setTitle(databaseRoom.title);
+      setQuestions(parsedQuestions);
+    })
+     
+
+  }, [roomId]);
+
+  return {questions, title}
+    
 }
--- letmeask/src/pages/Room.tsx
+++ letmeask/src/pages/Room.tsx
@@ -1,4 +1,4 @@
-import { FormEvent, useEffect, useState } from 'react';
+import { FormEvent, useState } from 'react';
 import toast from 'react-hot-toast';
 import { useParams } from 'react-router-dom';
 
@@ -8,30 +8,10 @@ import { Button } from '../components/Button';
 import { Question } from '../components/Question';
 import { RoomCode } from '../components/RoomCode';
 import { useAuth } from '../hooks/useAuth';
+import { useRoom } from '../hooks/useRoom';
 import { database } from '../services/firebase';
 
 import '../styles/room.scss';
-
-type QuestionType = {
-  id: string,
-  author: {
-    avatar: string;
-    name: string;
-  };
-  content: string;
-  isAnswered: boolean;
-  isHighLighted: boolean;
-}
-
-type FirebaseQuestions = Record<string, {
-  author: {
-    avatar: string;
-    name: string;
-  };
-  content: string;
-  isAnswered: boolean;
-  isHighLighted: boolean;
-}>
 
 type RoomParams = {
   id: string;
@@ -42,32 +22,9 @@ export function Room() {
   const params = useParams<RoomParams>();
   const roomId = params.id;
   const [newQuestion, setNewQuestion] = useState('');
-  const [questions, setQuestions] = useState<QuestionType[]>([])
-  const [title, setTitle] = useState('')
+  const { questions, title } = useRoom(roomId);
 
-  useEffect(() => {
-    const roomRef = database.ref(`rooms/${roomId}`);
-
-    roomRef.on('value', room => {
-      const databaseRoom = room.val();
-      const firebaseQuestions: FirebaseQuestions = databaseRoom.questions ?? {};
-
-      const parsedQuestions = Object.entries(firebaseQuestions).map(([key, value]) => {
-        return {
-          id: key,
-          author: value.author,
-          content: value.content,
-          isAnswered: value.isAnswered,
-          isHighLighted: value.isHighLighted,
-        }
-      })
-
-      setTitle(databaseRoom.title);
-      setQuestions(parsedQuestions);
-    })
-     
-
-  }, [roomId]);
+  
 
   async function handleSendQuestion(event: FormEvent) {
     event.preventDefault()
